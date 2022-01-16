dcdaeda9af5a5882251208fdfd2333b07e55aa10
cheehoong <lechho@gmail.com>
2022-01-16 23:11:43 +0800

    initial commit

--- pi-hole/CONTROL/post-install.sh
+++ pi-hole/CONTROL/post-install.sh
@@ -4,37 +4,8 @@ WEBPASSWORD=pihole
 WEB_PORT=3001
 ServerIP=$AS_NAS_INET4_IP1
 PIHOLE_FOLDER=/share/Docker/$APKG_PKG_NAME
-PIHOLE_CONF=$PIHOLE_FOLDER/etc-pihole
-PIHOLE_CONF1=$PIHOLE_FOLDER/etc-dnsmasq.d
-OLD_CONF=$APKG_PKG_DIR
 LOGGING=$PIHOLE_FOLDER/log.txt
 
-case "$APKG_PKG_STATUS" in
-	install)
-		# Make sure configuration directory exists
-		if [ ! -d "$PIHOLE_FOLDER" ]; then
-			mkdir "$PIHOLE_FOLDER"
-		fi
-
-		# Make sure configuration directory exists
-		if [ ! -d "$PIHOLE_CONF" ]; then
-			mkdir "$PIHOLE_CONF"
-			mkdir "$PIHOLE_CONF1"
-		fi
-		;;
-	upgrade)
-		# Make sure configuration directory exists
-		if [ ! -d "$OLD_CONF" ]; then
-			cp -r "$OLD_CONF/etc-pihole" $PIHOLE_FOLDER
-			cp -r "$OLD_CONF/etc-dnsmasq.d" $PIHOLE_FOLDER
-		fi
-		;;
-	*)
-		;;
-esac
-
-printf "OLD_CONF\n" >> $LOGGING
-printf "$OLD_CONF" >> $LOGGING
 printf "IP1\n" >> $LOGGING
 printf "$AS_NAS_INET4_IP1" >> $LOGGING
 printf "IP2\n" >> $LOGGING
--- pi-hole/CONTROL/pre-install.sh
+++ pi-hole/CONTROL/pre-install.sh
@@ -2,4 +2,43 @@
 
 echo "pre-install"
 
+PIHOLE_FOLDER=/share/Docker/$APKG_PKG_NAME
+PIHOLE_CONF=$PIHOLE_FOLDER/etc-pihole
+PIHOLE_CONF1=$PIHOLE_FOLDER/etc-dnsmasq.d
+OLD_CONF=$APKG_PKG_DIR/CONTROL
+LOGGING=$PIHOLE_FOLDER/log.txt
+
+case "$APKG_PKG_STATUS" in
+	install)
+		# Make sure configuration directory exists
+		printf "install\n" >> $LOGGING
+		if [ ! -d "$PIHOLE_FOLDER" ]; then
+			mkdir "$PIHOLE_FOLDER"
+		fi
+
+		# Make sure configuration directory exists
+		if [ ! -d "$PIHOLE_CONF" ]; then
+			mkdir "$PIHOLE_CONF"
+			mkdir "$PIHOLE_CONF1"
+		fi
+		;;
+	upgrade)
+		# Make sure configuration directory exists
+		printf "upgrade\n" >> $LOGGING
+		if [ ! -d "$PIHOLE_FOLDER" ]; then
+			mkdir "$PIHOLE_FOLDER"
+		fi
+
+		if [ ! -d "$OLD_CONF" ]; then
+			cp -r "$OLD_CONF/etc-pihole" $PIHOLE_FOLDER
+			cp -r "$OLD_CONF/etc-dnsmasq.d" $PIHOLE_FOLDER
+		fi
+		;;
+	*)
+		;;
+esac
+
+printf "OLD_CONF\n" >> $LOGGING
+printf "$OLD_CONF" >> $LOGGING
+
 docker pull pihole/pihole
